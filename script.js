3f76cd04a82b223205324ae28e601dd3b146fbea
yoosemry <mcyoose@gmail.com>
2023-05-13 12:15:22 +0300

    fixed header

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let closeIcon = 'fa-solid fa-xmark';
+
+const menu = document.querySelector('.main-menu');
+
+
+menu.addEventListener('click', function(){
+   document.querySelector('.links').classList.toggle('show');
+})
